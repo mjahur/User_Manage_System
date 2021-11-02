a48cfc0d463bcb95b822b37a5832ed7b49743a68
mjahur <mjahur@spartaglobal.com>
2021-11-02 10:54:08 +0000

    created project

---
+++ User_Management_System/User_Management_System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace User_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
