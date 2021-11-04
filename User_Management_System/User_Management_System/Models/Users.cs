f042608ce5b6b4d3fd83802b2208840fc9033a7f
mjahur <mjahur@spartaglobal.com>
2021-11-04 09:40:09 +0000

    updated database: changed Password to HashPassword and added Salt column, changed Users model accordingly

--- User_Management_System/User_Management_System/Models/Users.cs
+++ User_Management_System/User_Management_System/Models/Users.cs
@@ -7,12 +7,13 @@ namespace User_Management_System.Models
     {
         public int ID { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string HashPassword { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
         public string ProfilePicture { get; set; }
+        public string Salt { get; set;  }
     }
 }
