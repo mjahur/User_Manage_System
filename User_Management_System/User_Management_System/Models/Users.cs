f873bbde4c9b816084b8d06c99c4c4f7c7481716
mjahur <mjahur@spartaglobal.com>
2021-11-04 11:43:58 +0000

    added password(not mapped) into model in preparation for login page

--- User_Management_System/User_Management_System/Models/Users.cs
+++ User_Management_System/User_Management_System/Models/Users.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace User_Management_System.Models
 {
@@ -7,6 +9,13 @@ namespace User_Management_System.Models
     {
         public int ID { get; set; }
         public string Email { get; set; }
+
+        [NotMapped]
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
         public string HashPassword { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
